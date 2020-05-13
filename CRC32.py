78befdbf5b8e88ff2c591e7adf5e929af458f6df
nizaramadhan <65233535+nizaramadhan@users.noreply.github.com>
2020-05-13 20:37:30 +0700

    Create CRC32.py
    Tugas Mata Kuliah Telematika SMT4  - 1814321033

---
+++ CRC32.py
@@ -0,0 +1,29 @@
+from array import array
+
+poly = 0xEDB88320
+
+table = array('L')
+for byte in range(256):
+    crc = 0
+    for bit in range(8):
+        if (byte ^ crc) & 1:
+            crc = (crc >> 1) ^ poly
+        else:
+            crc >>= 1
+        byte >>= 1
+    table.append(crc)
+
+def encrypt(string):
+    bil = 0xffffffff
+
+    for ch in string:
+        bil = table[((ch) ^ bil) & 0x000000ff] ^ (bil >> 8)
+
+    return bil
+
+print("Program Menghitung CRC32")
+print("------------------------") 
+P = input("Input Hex :").encode("utf-8")
+
+print ("Result :        0x%08x" % (enkrip(P)))
+wait = input('Press Any Key')
